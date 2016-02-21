d388b84f3120e7b4cc8c52ea14fdd76cca63756a
Rodrigo Cardoso de Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2016-02-21 17:23:54 +0000

    Teste

--- estudo/index.php
+++ estudo/index.php
@@ -29,7 +29,7 @@ echo 'Hello world from Cloud9!';
                                 echo "</td>";
                             }
                             if ( $j % 2 != 0 ) {
-                                echo "<td>";
+                                echo "<td> $par";
                                 echo $par;
                                 echo "</td>";
                             }
